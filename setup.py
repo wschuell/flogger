a071fd7ec5ee219fde952873ac01230633f2123b
apere <alexandre.pere@inria.fr>
2019-01-22 16:16:03 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='flogger',
+      version='0.1',
+      description='Data logging made harder',
+      url='http://github.com/aPere3/flogger',
+      author='Alexandre Péré',
+      author_email='alexandre.pere@inria.fr',
+      license='MIT',
+      packages=['flogger'],
+      install_requires=[
+            "matplotlib",
+            "numpy",
+            "imageio",
+            "tensorboardX",
+      ],
+      zip_safe=False)
